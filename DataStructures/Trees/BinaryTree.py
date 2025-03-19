116e5f8146010297588ff0739bc8c4277e7aaea7
issac-kondreddy <ixk93660@ucmo.edu>
2025-03-19 16:52:08 -0500

    Traversal of Binary Tree

--- DataStructures/Trees/BinaryTree.py
+++ DataStructures/Trees/BinaryTree.py
@@ -16,7 +16,7 @@ class BinaryTree:
         else:
             queue = [self.root]
             while queue:
-                current = queue.pop()
+                current = queue.pop(0)
                 if current.left is None:
                     current.left = newnode
                     return f"Node Inserted {value}"
@@ -27,9 +27,37 @@ class BinaryTree:
                     return f"Node Inserted {value}"
                 else:
                     queue.append(current.right)
+    #Inorder Traversal
+    def inorder(self, root):
+        if root:
+            self.inorder(root.left)
+            print(root.data, end= " ")
+            self.inorder(root.right)
+    #PreOrder Traversal
+    def preorder(self, root):
+        if root:
+            print(root.data, end = " ")
+            self.preorder(root.left)
+            self.preorder(root.right)
+     #post order
+    def postorder(self, root):
+        if root:
+            self.postorder(root.left)
+            self.postorder(root.right)
+            print(root.data, end=" ")
+
+
 
 bt = BinaryTree()
 print(bt.insert(1))
 print(bt.insert(2))
 print(bt.insert(3))
 print(bt.insert(4))
+print("In Order Traversal : ")
+bt.inorder(bt.root)
+print()
+print("Pre Order Traversal : ")
+bt.preorder(bt.root)
+print()
+print("Post Order Traversal : ")
+bt.postorder(bt.root)
